89155d0f2af542563c7b5397f5d2e2f693d30de2
Guillaume Queste <guillaume.queste@laposte.net>
2021-05-02 09:47:13 +0200

    credits

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -1,30 +1,39 @@
-import React, { Component } from 'react'
-import Header from './Header'
-import HeaderMobile from './HeaderMobile'
-import Helmet from './Helmet'
-import Footer from './Footer'
+import React, { Component } from "react";
+import Header from "./Header";
+import HeaderMobile from "./HeaderMobile";
+import Helmet from "./Helmet";
+import Footer from "./Footer";
 
 class Credits extends Component {
-  
-    render () {
-        return (
-            <div>
-                <Helmet title={"Crédits Chevaux"}
-                        link={"http://chevaux.guillaumequeste.fr/credits"} />
-                <Header />
-                <HeaderMobile />
-                <Footer />
-                <div className="divCredits">
-                    <div className="divCredits2">
-                        <h5>Jérôme Ju</h5>
-                        <a href="https://www.facebook.com/Passioncourseshippiq-1538046549819119" target="_blank"><h5 className="textContact">Passion Courses Hippiques</h5></a>
-                        <a href="https://www.equidia.fr" target="_blank"><h5 className="textContact">Equidia</h5></a>
-                    </div>
-                </div>
-                <Footer />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Helmet
+          title={"Crédits Chevaux"}
+          link={"http://chevaux.guillaumequeste.fr/credits"}
+        />
+        <Header />
+        <HeaderMobile />
+        <Footer />
+        <div className="divCredits">
+          <div className="divCredits2">
+            <h5>Jérôme Ju</h5>
+            <a
+              href="https://www.facebook.com/Passioncourseshippiq-1538046549819119"
+              target="_blank"
+            >
+              <h5 className="textContact">Passion Courses Hippiques</h5>
+            </a>
+            <a href="https://www.equidia.fr" target="_blank">
+              <h5 className="textContact">Equidia</h5>
+            </a>
+            <h5>Pierre Virly (Petrus photo)</h5>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
-export default Credits
+export default Credits;
