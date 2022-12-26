774203c7457abbd949cd4ca5d62fe4edb4ef3510
Guillaume Queste <guillaume.queste@laposte.net>
2022-12-26 19:33:11 +0100

    26 decembre 2022

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,7 +24,7 @@ class Contact extends Component {
             <br></br>
             <h5 className="textContact">guillaume.queste@laposte.net</h5>
             <br></br>
-            <h6>dernière mise à jour : 4 décembre 2022</h6>
+            <h6>dernière mise à jour : 26 décembre 2022</h6>
           </div>
         </div>
       </div>
